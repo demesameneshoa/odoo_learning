49de7d7e762b146ca7370bc069c9ac87c294b2f7
demesameneshoa <demesameneshoa@gmail.com>
2026-01-21 23:21:10 +0300

    created manifest and init files

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Real Estate Module Learning",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Demes Abera",
+    'category': 'Administration',
+    'description': """
+    This is a module i created by following up the odoo documentation to create a new module
+    """,
+    # data files always loaded at installation
+    'data': [
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+}
